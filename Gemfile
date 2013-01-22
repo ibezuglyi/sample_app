4ccef9ecae55887df5e352fa8d9472034f50aad2
ibezuglyi <ievgen.bezuglyi@gmail.com>
2013-01-22 21:46:41 +0100

    init

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'sqlite3', '1.3.4'
+
+group :development do 
+	gem 'rspec-rails', '2.6.1'
+end
+
+group :test do
+	gem 'rspec-rails', '2.6.1'
+	gem 'webrat', '0.7.1'
+end
+
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ca5c13ee45a0989bbd6e21be195b5c9ca8bd5c8c35101c1217061e7ae5ae11b67472db77a7a8b0d716908c3ac018a4dc5de04bfe7e1c229136e41294a7d372f9'
